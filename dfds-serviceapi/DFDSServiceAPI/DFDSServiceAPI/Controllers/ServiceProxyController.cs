75c331165d9971a3924688852c677277659574dc
Richard Fisher <rifis@dfds.com>
2021-03-01 15:45:37 +0000

    WIP. I forgot how to program

---
+++ dfds-serviceapi/DFDSServiceAPI/DFDSServiceAPI/Controllers/ServiceProxyController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using DFDSServiceAPI.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DFDSServiceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceProxyController : Controller
+    {
+        private readonly IServiceProxy _proxy;
+        private readonly IMapper _mapper;
+
+        public ServiceProxyController(IServiceProxy proxy, IMapper mapper)
+        {
+            _proxy = proxy;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var results = _mapper.Map<ServiceProxyResultDto>(await _proxy.GetResults());
+            return Ok(results);
+        }
+    }
+}
---
+++ dfds-serviceapi/DFDSServiceAPI/DFDSServiceAPI/Dtos/ServiceProxyResultDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DFDSServiceAPI.Dtos
+{
+    public class ServiceProxyResultDto
+    {
+        public string result { get; set; }
+    }
+}
