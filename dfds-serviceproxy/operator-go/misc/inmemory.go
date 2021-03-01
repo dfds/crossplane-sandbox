bb6e1ad94f1ebc2ec44acb06dc4de9797168657a
Richard Fisher <rifis@dfds.com>
2021-03-01 11:47:13 +0000

    added some output for console

--- dfds-serviceproxy/operator-go/misc/inmemory.go
+++ dfds-serviceproxy/operator-go/misc/inmemory.go
@@ -1,6 +1,8 @@
 package misc
 
 import (
+	"fmt"
+
 	v1 "k8s.io/api/core/v1"
 	v1Networking "k8s.io/api/networking/v1beta1"
 )
@@ -21,6 +23,8 @@ func NewInMemoryStore() *InMemoryStore {
 
 func (s *InMemoryStore) PutService(key string, val *v1.Service) {
 	s.Services[key] = val
+	fmt.Println(string("\033[32m"), "Added: ", key)
+	fmt.Println(string("\033[34m"), "Values: ", val, string("\033[0m"))
 }
 
 func (s *InMemoryStore) GetService(key string) *v1.Service {
@@ -39,6 +43,8 @@ func (s *InMemoryStore) RemoveService(key string) {
 
 func (s *InMemoryStore) PutIngress(key string, val *v1Networking.Ingress) {
 	s.Ingresses[key] = val
+	fmt.Println(string("\033[32m"), "Added: ", key)
+	fmt.Println(string("\033[34m"), "Values: ", val, string("\033[0m"))
 }
 
 func (s *InMemoryStore) GetIngress(key string) *v1Networking.Ingress {
